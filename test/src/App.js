e279cb60414dcaf133a342d751db4c14766fe8dd
hitit <hitit1000@naver.com>
2022-07-01 19:05:50 +0900

    test tsx,scss

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,10 @@
-
+import Header from "./component/Header.tsx"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- test/src/component/Header.tsx
+++ test/src/component/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./Header.scss"
 
 export default function Header() {
   return (
     <>
-      <nav> 
+      <nav className='Header_nav'> 
         <div>Header</div>
         <span>Item1</span>
         <span>Item2</span>
