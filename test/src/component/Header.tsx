5cf742663309e96f368e38443b4edfc94774eca6
hitit <hitit1000@naver.com>
2022-07-01 18:59:03 +0900

    test_setup setting

---
+++ test/src/component/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <nav> 
+        <div>Header</div>
+        <span>Item1</span>
+        <span>Item2</span>
+        <span>Item3</span>
+      </nav>
+    </>
+  )
+}
